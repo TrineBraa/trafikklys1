a860b7be136b1c05eece1217bd283f11f0ab1e62
trinebra <157688079+trinebra@users.noreply.github.com>
2024-02-16 13:55:55 +0100

    Update

---
+++ script.js
@@ -0,0 +1,34 @@
+timer;
+
+function makeRed() {
+    document.getElementById("redLight").classList.add("activeLight");
+    document.getElementById("yellowLight").classList.remove("activeLight")
+    document.getElementById("greenLight").classList.remove("activeLight")
+    timer = setTimeout(makeYellow, 4000)
+}
+
+function makeYellow() {
+    document.getElementById("yellowLight").classList.add("activeLight");
+    timer = setTimeout(makeGreen, 2000)
+}
+
+function makeGreen() {
+    document.getElementById("greenLight").classList.add("activeLight");
+    document.getElementById("yellowLight").classList.remove("activeLight")
+    document.getElementById("redLight").classList.remove("activeLight")
+    timer = setTimeout(makeYellow2, 40000)
+}
+
+function makeYellow2() {
+    document.getElementById("yellowLight").classList.add("activeLight");
+    timer = setTimeout(makeRed, 2000)
+}
+
+function startTheLight() {
+    makeRed()
+}
+
+function reloadTheLight() {
+    location.reload()
+}
+
